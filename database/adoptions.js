0da0a4c87288c6e061b465dad8c9ddcd657240fa
ddmoreno1 <ddmoreno1@espe.edu.ec>
2024-01-23 18:20:05 -0500

    sin git

---
+++ database/adoptions.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    "id": 1,
+    "dogId": 1,
+    "adopterId": 1
+  },
+  {
+    "id": 2,
+    "dogId": "1",
+    "adopterId": "2"
+  },
+  {
+    "id": 3,
+    "dogId": "1",
+    "adopterId": "2"
+  },
+  {
+    "id": 4,
+    "dogId": "1",
+    "adopterId": "1"
+  },
+  {
+    "id": 13,
+    "dogId": "3",
+    "adopterId": "4"
+  }
+];
---
+++ frontend/src/components/AddDogForm.jsx
@@ -0,0 +1,30 @@
+// AddDogForm.jsx
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const AddDogForm = ({ onDogSubmit }) => {
+    const [dogName, setDogName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (dogName) {
+            onDogSubmit(dogName);
+            setDogName('');
+        } else {
+            console.error('Debe ingresar una pregunta.');
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={dogName} onChange={(e) => setDogName(e.target.value)} placeholder="Nombre de la Evaluacion" />
+            <button type="submit">Agregar Evaluacion</button>
+        </form>
+    );
+};
+
+AddDogForm.propTypes = {
+    onDogSubmit: PropTypes.func.isRequired,
+};
+
+export default AddDogForm;
